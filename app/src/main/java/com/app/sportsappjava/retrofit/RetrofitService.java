9dcf066c94e39145da3483c11937d95100c579cc
Dinesh2510 <dineshchavan7708@outlook.com>
2022-11-14 15:24:39 +0530

    removed unused class

--- app/src/main/java/com/app/sportsappjava/retrofit/RetrofitService.java
+++ app/src/main/java/com/app/sportsappjava/retrofit/RetrofitService.java
@@ -11,9 +11,6 @@ public interface RetrofitService {
 
     @GET("nzin01312019187360.json")
     Call<ResponseGame> getGameData();
-    /*
-    https://demo.sportz.io/sapk01222019186652.json
-        https://demo.sportz.io/nzin01312019187360.json
-    * */
+
 }
 
